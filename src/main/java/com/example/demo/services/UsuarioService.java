fecd6ac672acc7c9015d27db1f3d586be457bd5c
Mateo Rodriguez <mateoytomas2009@hotmail.com>
2022-02-17 22:22:45 -0300

    Implementacion de metodos obtenerUsuarios y guardarUsuario en el archivo UsuarioService junto a sus respectivos GetMapping y PostMapping en el archivo UsuarioController

---
+++ src/main/java/com/example/demo/services/UsuarioService.java
@@ -0,0 +1,21 @@
+package com.example.demo.services;
+
+import java.util.ArrayList;
+import com.example.demo.models.UsuarioModel;
+import com.example.demo.repositories.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsuarioService {
+    @Autowired
+    UsuarioRepository usuarioRepository;
+
+    public ArrayList<UsuarioModel> obtenerUsuarios(){
+        return (ArrayList<UsuarioModel>) usuarioRepository.findAll();
+    }
+
+    public UsuarioModel guardarUsuario(UsuarioModel usuario){
+        return usuarioRepository.save(usuario);
+    }
+}
