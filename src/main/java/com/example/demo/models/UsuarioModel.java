07de7ddff62ff480731fb3ead462c2f71885f47c
Mateo Rodriguez <mateoytomas2009@hotmail.com>
2022-02-17 21:52:19 -0300

    Creacion springboot, modelo usuario y coneccion con base de datos

---
+++ src/main/java/com/example/demo/models/UsuarioModel.java
@@ -0,0 +1,48 @@
+package com.example.demo.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "usuario")
+public class UsuarioModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true, nullable = false)
+    private long id;
+    private String nombre;
+    private String email;
+    private Integer prioridad;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getPrioridad() {
+        return prioridad;
+    }
+
+    public void setPrioridad(Integer prioridad) {
+        this.prioridad = prioridad;
+    }
+}
